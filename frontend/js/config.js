a5e905d603e1e98b952a54790a4e72f9e28b777a
jenrick484-sudo <montelee282@gmail.com>
2026-08-27 18:47:21 +0800

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -2,5 +2,5 @@ const CONFIG = {
   // Awtomatikong pinipili ang URL depende kung local o live site
   API_BASE_URL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://localhost:5000/api'
-    : 'https://auto-parts-pos-9pv5.onrender.com' // Papalitan ito pagkatapos mai-deploy ang backend sa Render
+    : 'https://auto-parts-pos-9pv5.onrender.com/api' // Papalitan ito pagkatapos mai-deploy ang backend sa Render
 };
